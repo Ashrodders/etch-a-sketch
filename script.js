e7c52c36b02ceedca6c693c025d5fa50fe74882a
Ash <ashrodders@gmail.com>
2024-09-09 21:01:43 +0100

    i made changes to the plans of the project may have to delete this, trying out pseudocode 1st attempt

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const container1 = document.querySelector('.container');
+// Select the container where the squares will go
+const container = document.querySelector('#container');
 
+// Create a 16x16 grid of squares
 for (let i = 0; i < 16 * 16; i++) {
     const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+    square.classList.add('square'); // Add the 'square' class to each div
+    container.appendChild(square);  // Append the square to the container
 }
